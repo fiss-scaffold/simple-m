546367ea9be4ef3a04d027f4d9d664afeab029e9
zhangyihua <shanzimr@163.com>
2016-02-26 18:33:19 +0800

    add fis-conf.js

---
+++ fis-conf.js
@@ -0,0 +1,31 @@
+/**
+ * fis-conf template
+ *
+ */
+
+
+// dev match
+fis
+.match('your pattern1', {
+	// TODO
+})
+.match('your pattern2', {
+	// TODO
+});
+// and more
+
+// debug
+fis.media('debug').match('', {
+	// TODO
+});
+
+// test
+fis.media('test').match('', {
+
+});
+
+// pro
+fis.media('pro').match('', {
+	// TODO
+});
+
